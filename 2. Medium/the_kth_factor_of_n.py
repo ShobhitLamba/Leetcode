2b2bde1d97fe4263e41067e6caf1d65ece3e0fe7
Shobhit Lamba <shobhitlamba.ynwa@gmail.com>
2022-03-07 11:53:58 -0600

    Medium

---
+++ 2. Medium/the_kth_factor_of_n.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        for x in range(1, n // 2 + 1):
+            if n % x == 0:
+                k -= 1
+                if k == 0:
+                    return x
+
+        return n if k == 1 else -1
